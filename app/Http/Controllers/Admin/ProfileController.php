a72ba2726c9b4c2fe61dee7c5f2f0b98f2690ce5
EC2 Default User <ec2-user@ip-172-31-31-192.ap-southeast-2.compute.internal>
2023-08-08 03:59:21 +0000

    PHP/Laravel14 課題を更新

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -30,6 +30,23 @@ class ProfileController extends Controller
     
      public function profile(Request $request)
      {
+         // 以下を追記
+        // Validationを行う
+        $this->validate($request, Plofile::$rules);
+
+        $profiles = new Profile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $profiles->fill($form);
+        $profiles->save();
+
+         
          // admin/profile/createにリダイレクトする
         return redirect('admin/profile/create');
     }
